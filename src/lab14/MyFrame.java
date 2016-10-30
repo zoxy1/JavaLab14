9ef68fe8b03f1760713d4ba1c709da1039612e01
Andrey <zoxy1@yandex.ru>
2016-10-30 23:41:14 +0700

    finish

--- src/lab14/MyFrame.java
+++ src/lab14/MyFrame.java
@@ -1,44 +1,14 @@
 package lab14;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTree;
-import javax.swing.ListModel;
 import javax.swing.border.EmptyBorder;
-
-import java.awt.Color;
-
 import javax.swing.UIManager;
-import javax.swing.JPopupMenu;
-
-import java.awt.Component;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-import javax.swing.JMenuBar;
-
-import java.awt.Panel;
-import java.awt.ScrollPane;
-import java.awt.Checkbox;
-import java.awt.List;
-import java.awt.Choice;
-import java.awt.FlowLayout;
-
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-
-import java.awt.Label;
-
-import javax.swing.JCheckBoxMenuItem;
-
-import java.awt.event.ItemListener;
-import java.awt.event.ItemEvent;
-
 import javax.swing.JButton;
 
 import java.awt.event.ActionListener;
@@ -47,20 +17,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 
-import javax.swing.JList;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeModel;
 
 public class MyFrame extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	final JButton btnOk = new JButton("Ok");
 	File file;
@@ -99,8 +66,7 @@ public class MyFrame extends JFrame {
 
 		ArrayList<String> stringList = new ArrayList<>();
 		// использование try-with-resources
-		try (BufferedReader reader = new BufferedReader(
-				new FileReader(fileName))) {
+		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
 			String line;
 			while ((line = reader.readLine()) != null) {
 
@@ -135,7 +101,7 @@ public class MyFrame extends JFrame {
 		setTitle("Lab14");
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 600);
+		setBounds(100, 100, 820, 600);
 		contentPane = new JPanel();
 		contentPane.setBackground(UIManager.getColor("OptionPane.background"));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -171,11 +137,19 @@ public class MyFrame extends JFrame {
 							// в
 							// Double и
 							// добавляем в список doubleList
-							for (int j = 0; j < isParts.length; j++) {
-								doubleList.add(j,
-										Double.parseDouble(isParts[j]));
 
+							for (int j = 0; j < isParts.length; j++) {
+								try {
+									doubleList.add(j, Double.parseDouble(isParts[j]));
+								} catch (NumberFormatException ewr) {
+
+									System.out.println("Выбранный файл содержит не корректные данные");
+									contentPane.setVisible(false);
+									System.exit(0);
+
+								}
 							}
+
 							// минимальное значение аргумента х которое
 							// находится в
 							// списке
@@ -188,9 +162,7 @@ public class MyFrame extends JFrame {
 							// х
 							while (x <= doubleList.get(5)) {
 								// вычисляем уравнение
-								y = variableCalculation.calculateFunction(
-										doubleList.get(0), doubleList.get(1),
-										doubleList.get(2), doubleList.get(3), x);
+								y = variableCalculation.calculateFunction(doubleList.get(0), doubleList.get(1), doubleList.get(2), doubleList.get(3), x);
 								// выводим значения уравнения для всег
 								// аргументов от
 								// минимального до максимального значения с
@@ -224,62 +196,29 @@ public class MyFrame extends JFrame {
 						// метода writeFile,
 						// который сам обрабатывает исключения
 						MyFrame.writeFile("src/out.txt", sb.toString());
-						// записываем полученные результаты в файл out.txt,
+						System.out.println("Полученные результаты расчетов записаны в файл out.txt");
 
-					} else {
-						System.out.println("Файл не выбран!!!");
 					}
 				}
 				if (file.isDirectory()) {
 
-					Path testFilePath = Paths.get(file.getAbsolutePath());
+					TreeModel model = new FileTreeModel(file);
+					JTree tree_1 = new JTree(model);
+					JScrollPane scrollPane = new JScrollPane(tree_1);
+					scrollPane.setViewportBorder(new EmptyBorder(1, 1, 1, 1));
+					scrollPane.setToolTipText("Scroll panell");
+					scrollPane.setBounds(5, 60, 800, 500);
+					contentPane.add(scrollPane);
 
-					for (Path element : testFilePath) {
-						System.out.println("\t path element: " + element);
-					}
-
-					DefaultMutableTreeNode books = new DefaultMutableTreeNode(
-							testFilePath.getRoot());
-
-					// Three Departments
-					DefaultMutableTreeNode fiction = new DefaultMutableTreeNode(
-							"Fiction");
-					DefaultMutableTreeNode nonfiction = new DefaultMutableTreeNode(
-							"Non-Fiction");
-					DefaultMutableTreeNode biography = new DefaultMutableTreeNode(
-							"Biography");
-
-					// Fiction Books
-					fiction.add(new DefaultMutableTreeNode("Moby Dick"));
-					fiction.add(new DefaultMutableTreeNode("MacBeth"));
-					fiction.add(new DefaultMutableTreeNode("War and Peace"));
-
-					// Non Fiction Books
-					nonfiction.add(new DefaultMutableTreeNode("Unbroken"));
-					nonfiction.add(new DefaultMutableTreeNode(
-							"The Diary of a Young Girl"));
-					nonfiction.add(new DefaultMutableTreeNode("The Prince"));
-
-					// Biography Books
-					biography.add(new DefaultMutableTreeNode("John Adams"));
-					biography.add(new DefaultMutableTreeNode("Steve Jobs"));
-
-					books.add(fiction);
-					books.add(nonfiction);
-					books.add(biography);
-
-					JTree tree_1 = new JTree(books);
-					tree_1.setBounds(10, 110, 420, 424);
-
-					contentPane.add(tree_1);
 					contentPane.updateUI();
+
 				}
 			}
 		});
 
 		btnOk.setBounds(131, 25, 97, 25);
 		contentPane.add(btnOk);
-		JButton selectFileButton = new JButton("Select File");
+		JButton selectFileButton = new JButton(" Select");
 		selectFileButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
@@ -287,15 +226,13 @@ public class MyFrame extends JFrame {
 
 				fileopen.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 
-				int ret = fileopen.showDialog(null, "Открыть");
+				int ret = fileopen.showDialog(null, "Open");
 				if (ret == JFileChooser.APPROVE_OPTION) {
 
 					file = fileopen.getSelectedFile();
 
-					// void setDialogType(int dialogType)
 					if (file.isDirectory()) {
-						System.out.println("Выбрана директория: "
-								+ file.getAbsolutePath());
+						System.out.println("Выбрана директория: " + file.getAbsolutePath());
 
 					}
 					if (file.isFile()) {
@@ -303,10 +240,8 @@ public class MyFrame extends JFrame {
 					}
 					btnOk.setEnabled(true);
 				} else {
-					System.out.println("Файл или директория не выбрны");
+					System.out.println("Файл или директория не выбраны");
 					btnOk.setEnabled(false);
-
-					// System.out.println(fileopen.getRootPane().getD);
 
 				}
 			}
@@ -324,20 +259,6 @@ public class MyFrame extends JFrame {
 		});
 		btnCancel.setBounds(252, 25, 97, 25);
 		contentPane.add(btnCancel);
-
-		// String[] elements = new String[] {"Вася", "Петя",
-		// "<html><font size = +1 color = yellow>Иван</font>"};
-		//
-		//
-		// JComboBox comboBox = new JComboBox(elements);
-		// comboBox.setSelectedIndex(1);
-		// comboBox.setSize(getSize());
-		// comboBox.setEditable(true);
-		// comboBox.setBounds(161, 63, 31, 22);
-		//
-		//
-		//
-		// contentPane.add(comboBox);
 
 	}
 }
